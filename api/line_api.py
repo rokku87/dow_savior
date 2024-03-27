8e420d5ce0c0b38e5a16e5c4552476e0772b98a9
rokku87 <j810331@gmail.com>
2024-03-28 01:16:46 +0800

    Update line_api.py

--- api/line_api.py
+++ api/line_api.py
@@ -1,7 +1,7 @@
 # api/line_api.py
+from linebot import LineBotApi
 from linebot.models import TextSendMessage
 from linebot.v3.messaging import ApiClient, MessagingApi, ReplyMessageRequest, TextMessage
-from linebot.models import TextSendMessage
 
 def reply_message(token, reply_token, message_text, config):
     with ApiClient(config) as api_client:
@@ -13,8 +13,7 @@ def reply_message(token, reply_token, message_text, config):
         line_bot_api.reply_message_with_http_info(reply_request)
 
 
-def send_message(channel_access_token, user_id, message_text, configuration):
-    with ApiClient(configuration) as api_client:
-        line_bot_api = MessagingApi(api_client)
-        message = TextSendMessage(text=message_text)  # 创建文本消息对象
-        line_bot_api.push_message(user_id, [message])  # 注意：第二个参数是消息对象的列表
+def send_message(channel_access_token, user_id, message_text):
+    line_bot_api = LineBotApi(channel_access_token)
+    message = TextSendMessage(text=message_text)
+    line_bot_api.push_message(user_id, messages=[message])
