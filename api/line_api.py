dbf3d496829f5c3dc1461eccc52171f98a9b7466
rokku87 <49755534+rokku87@users.noreply.github.com>
2024-03-27 22:43:58 +0800

    Add files via upload

--- api/line_api.py
+++ api/line_api.py
@@ -1,5 +1,7 @@
 # api/line_api.py
+from linebot.models import TextSendMessage
 from linebot.v3.messaging import ApiClient, MessagingApi, ReplyMessageRequest, TextMessage
+from linebot.v3.models import TextSendMessage
 
 def reply_message(token, reply_token, message_text, config):
     with ApiClient(config) as api_client:
@@ -9,3 +11,13 @@ def reply_message(token, reply_token, message_text, config):
             messages=[TextMessage(text=message_text)]
         )
         line_bot_api.reply_message_with_http_info(reply_request)
+
+
+def send_message(channel_access_token, user_id, message_text, configuration):
+    # 使用给定的配置创建 API 客户端
+    with ApiClient(configuration) as api_client:
+        line_bot_api = MessagingApi(api_client)
+        # 创建消息对象
+        message = TextSendMessage(text=message_text)
+        # 调用 LINE Messaging API 发送消息
+        line_bot_api.push_message(user_id, message)
