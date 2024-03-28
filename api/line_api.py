de41cd0bd6b66457fb3e096ca04190a8d02553ab
rokku87 <j810331@gmail.com>
2024-03-28 12:53:48 +0800

    Update line_api.py

--- api/line_api.py
+++ api/line_api.py
@@ -1,22 +1,15 @@
 # api/line_api.py
 from linebot import LineBotApi
-from linebot.models import TemplateSendMessage, ConfirmTemplate, MessageAction
+from linebot.models import TextSendMessage, TemplateSendMessage, ConfirmTemplate, MessageAction
 
 def send_message(channel_access_token, user_id, message):
     line_bot_api = LineBotApi(channel_access_token)
-    line_bot_api.push_message(user_id, message)
+    if isinstance(message, (TextSendMessage, TemplateSendMessage)):
+        line_bot_api.push_message(user_id, message)
+    else:
+        raise TypeError("Message must be either TextSendMessage or TemplateSendMessage")
 
-# 如果還需要回覆消息的功能，可以保留這個函數
-def reply_message(token, reply_token, message_text, config):
+def reply_message(token, reply_token, message_text):
     line_bot_api = LineBotApi(token)
-    message = TemplateSendMessage(
-        alt_text='回覆消息',
-        template=ConfirmTemplate(
-            text=message_text,
-            actions=[
-                MessageAction(label='是', text='是'),
-                MessageAction(label='否', text='否')
-            ]
-        )
-    )
+    message = TextSendMessage(text=message_text)
     line_bot_api.reply_message(reply_token, message)
