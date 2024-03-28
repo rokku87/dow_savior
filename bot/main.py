5c7dd225ce27a21cc4921583e8830603c0ee4a75
rokku87 <j810331@gmail.com>
2024-03-28 13:25:15 +0800

    Update main.py

--- bot/main.py
+++ bot/main.py
@@ -1,5 +1,6 @@
 import os
 import sys
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 from datetime import datetime, timedelta
 from linebot import LineBotApi
 from linebot.models import TemplateSendMessage, ConfirmTemplate, MessageAction, TextSendMessage
